9b5b6cfb6e206c5a367bf1b4ddfe254831bd49be
Divya <divyarupajandhyala@msitprogram.net>
2021-04-12 23:24:35 +0530

    null case handled

--- Javacript/script.js
+++ Javacript/script.js
@@ -1,10 +1,22 @@
+// function deepEqual(a,b){
+//     if(a===b)
+//             return true;
+//     else 
+//         return false;
+// }
 function deepEqual(a,b){
     if(typeof a==typeof b){
-        if(a===b)
+        if ((typeof a=='object' && typeof b == 'object') && (a==null && b == null))
             return true;
+        
+        else if((typeof a=='object' && typeof b == 'object') && (a==null || b == null))
+            return false ;
+        else
+            return true
     }
         
     else
         return false;
 }
-console.log(deepEqual(1,"2"))
+console.log(deepEqual(1,"1"))
+// console.log(typeof {})
