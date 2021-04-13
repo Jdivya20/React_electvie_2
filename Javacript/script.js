f40939a4440ea698fd626f728ed2f0020104c806
Divya <divyarupajandhyala@msitprogram.net>
2021-04-13 08:55:28 +0530

    handled same type byt different value error

--- Javacript/script.js
+++ Javacript/script.js
@@ -6,17 +6,20 @@
 // }
 function deepEqual(a,b){
     if(typeof a==typeof b){
-        if ((typeof a=='object' && typeof b == 'object') && (a==null && b == null))
+        if(a==b)
+            return true
+        else if ((typeof a=='object' && typeof b == 'object') && (a==null && b == null))
             return true;
         
         else if((typeof a=='object' && typeof b == 'object') && (a==null || b == null))
             return false ;
         else
-            return true
+            return false
+       
     }
         
     else
         return false;
 }
-console.log(deepEqual(1,"1"))
+console.log(deepEqual("ab","a"))
 // console.log(typeof {})
